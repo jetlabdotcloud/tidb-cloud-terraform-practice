df6694987f310afa638fb61f07a3f4ab2c9d96e8
JetSquirrel <dengtian1997@outlook.com>
2024-04-20 15:46:21 +0800

    init

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    tidbcloud = {
+      source = "tidbcloud/tidbcloud"
+    }
+  }
+
+  cloud {
+    organization = "jetlabdotcloud"
+    workspaces {
+      name = "tidb-terraform-practice"
+    }
+  }
+}
+
+provider "tidbcloud" {
+  public_key = vars.tidb_public_key
+  private_key = vars.tidb_private_key
+}
+
+resource "tidbcloud_cluster" "jetlab" {
+  project_id     = "001"
+  name           = "jetlab-cluster"
+  cluster_type   = "DEVELOPER"
+  cloud_provider = "AWS"
+  region         = "ap-southeast-1"
+  config = {
+    root_password = vars.tidb_root_password
+    port = 4000
+
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "tidb_public_key" {
+  type = string
+}
+variable "tidb_private_key" {
+  type = string
+}
+variable "tidb_root_password" {
+  type = string
+}
